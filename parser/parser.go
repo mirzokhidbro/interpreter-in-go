dca4a7b822b1196159582fd31ac4a678b8530bf5
“mirzoxid” <mirzoxidadxamjanov01@gmail.com>
2025-08-15 23:13:56 +0500

    evaluating conditional

--- parser/parser.go
+++ parser/parser.go
@@ -157,6 +157,14 @@ func (p *Parser) parseIfExpression() ast.Expression {
 		return nil
 	}
 	expression.Consequence = p.parseBlockStatement()
+
+	if p.peekTokenIs(token.ELSE) {
+		p.nextToken()
+		if !p.expectPeek(token.LBRACE) {
+			return nil
+		}
+		expression.Alternative = p.parseBlockStatement()
+	}
 	return expression
 }
 
